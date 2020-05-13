109fa409674a79c7d034226509056868348614d5
unknown <a_eker1@hotmail.comgit config --global user.name abigali1>
2020-05-13 15:57:53 +0100

    update CreateChanneljava

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,6 +3,12 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
        System.out.println("User is able to create channel ");
+
+        int x=0; // changed from intellij
+        if (x==0){
+            System.out.println("equal");
+
+        }
     }
 }
 
